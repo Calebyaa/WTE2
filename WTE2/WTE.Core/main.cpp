c41a37e0056578c3499f463822793c314f2dc80f
Calebyaa <caleb@calebvarty.com>
2018-11-19 23:57:19 -0500

    now drawing a window. clears to orange, but need to find the old WTE orange

--- WTE2/WTE.Core/main.cpp
+++ WTE2/WTE.Core/main.cpp
@@ -1,5 +1,40 @@
+#include <iostream>
+
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#ifndef WTE_MAIN
+
 int main() {
-	glfwInit();
+
+	if (!glfwInit()) {
+		exit(EXIT_FAILURE);
+	}
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_DOUBLEBUFFER, GL_TRUE);
+
+	auto window = glfwCreateWindow(1600, 900, "Working Title Engine 2", NULL, NULL);
+
+	if (nullptr == window) {
+		glfwTerminate();
+		exit(EXIT_FAILURE);
+	}
+
+	glfwMakeContextCurrent(window);
+	gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+	glfwSwapInterval(0);
+
+	glClearColor(0.96f, 0.36f, 0.15f, 1.0f);
+
+	// simplest draw loop, maybe?
+	do {
+		glClear(GL_COLOR_BUFFER_BIT);
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	} while (!glfwWindowShouldClose(window) && glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS);
+
 }
+
+#endif // !WTE_MAIN
