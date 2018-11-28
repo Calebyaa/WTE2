797a005b4a28d198770da298b5dc3499272e24f1
Calebyaa <caleb@calebvarty.com>
2018-11-28 17:49:22 -0500

    WTE.Graphics now generates a lib. Separate project calls it.

--- WTE.Project.ProofOfConcept/ProofOfConcept/main.cpp
+++ WTE.Project.ProofOfConcept/ProofOfConcept/main.cpp
@@ -1,20 +1,9 @@
+#include <wte/graphics/graphics.h>
 #include <memory>
 
-#include <wte/graphics/graphics.h>
-
-#ifndef WTE_MAIN
-
 int main() {
-
     auto renderer = std::make_shared<wte::graphics::Renderer>();
-    
     renderer->Start();
-    
-    while (renderer->Render()) {}
-
+    while(renderer->Render()) {}
     renderer->Shutdown();
-
-    exit(EXIT_SUCCESS);
 }
-
-#endif // !WTE_MAIN
